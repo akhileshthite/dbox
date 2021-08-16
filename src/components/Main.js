1f6c64025e49551661c300924459f2bf2e60e450
Akhilesh Thite <akhithite@gmail.com>
2021-08-16 14:54:43 +0530

    dbox

---
+++ src/components/Main.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import { convertBytes } from './helpers';
+import moment from 'moment'
+import './App.css';
+
+class Main extends Component {
+
+  render() {
+    return (
+      <div className="container-fluid mt-5 text-center">
+        <div className="row">
+          <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '1024px' }}>
+            <div className="content">
+              <p>&nbsp;</p>
+              <div className="card mb-3 mx-auto bg-dark" style={{ maxWidth: '512px' }}>
+                <h2 className="heading">Share File 📄</h2>
+                  <form onSubmit={(event) => {
+                    event.preventDefault()
+                    const description = this.fileDescription.value
+                    this.props.uploadFile(description)
+                  }} >
+                      <div className="input form-group">
+                        <br></br>
+                          <input
+                            id="fileDescription"
+                            type="text"
+                            ref={(input) => { this.fileDescription = input }}
+                            className="form-control"
+                            placeholder="File description..."
+                            required />
+                      </div>
+                    <input type="file" onChange={this.props.captureFile} className="text-white text-monospace"/>
+                    <button type="submit" className="upload btn-primary">Upload</button>
+                  </form>
+              </div>
+              <p>&nbsp;</p>
+              <table className="table-sm table-bordered text-monospace" style={{ width: '1000px', maxHeight: '450px'}}>
+                <thead style={{'fontSize':'15px', 'font-weight':'lighter'}}>
+                  <tr className="table">
+                    <th scope="col" style={{ width: '10px'}}>Id</th>
+                    <th scope="col" style={{ width: '200px'}}>Name</th>
+                    <th scope="col" style={{ width: '230px'}}>Description</th>
+                    <th scope="col" style={{ width: '120px'}}>Type</th>
+                    <th scope="col" style={{ width: '90px'}}>Size</th>
+                    <th scope="col" style={{ width: '90px'}}>Date</th>
+                    <th scope="col" style={{ width: '120px'}}>Uploader</th>
+                    <th scope="col" style={{ width: '120px'}}>Share</th>
+                  </tr>
+                </thead>
+                { this.props.files.map((file, key) => {
+                  return(
+                    <thead style={{ 'fontSize': '12px' }} key={key}>
+                      <tr className="table-content">
+                        <td>{file.fileId}</td>
+                        <td>{file.fileName}</td>
+                        <td>{file.fileDescription}</td>
+                        <td>{file.fileType}</td>
+                        <td>{convertBytes(file.fileSize)}</td>
+                        <td>{moment.unix(file.uploadTime).format('h:mm:ss A M/D/Y')}</td>
+                        <td>
+                          <a
+                            href={"https://etherscan.io/address/" + file.uploader}
+                            rel="noopener noreferrer"
+                            target="_blank">
+                            {file.uploader.substring(0,10)}...
+                          </a>
+                         </td>
+                        <td>
+                          <a
+                            href={"https://ipfs.infura.io/ipfs/" + file.fileHash}
+                            rel="noopener noreferrer"
+                            target="_blank">
+                            {file.fileHash.substring(0,10)}...
+                          </a>
+                        </td>
+                      </tr>
+                    </thead>
+                  )
+                })}
+              </table>
+            </div>
+          </main>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
---
+++ truffle-config.js
@@ -0,0 +1,33 @@
+require('babel-register');
+require('babel-polyfill');
+require('dotenv').config();
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    },
+    matic: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://rpc-mumbai.matic.today`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      gas: 6000000,
+      gasPrice: 10000000000,
+    },
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
